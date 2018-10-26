8d4c261a5dc19e1c7bb8274633cc3a90e039bf3e
League Student <league@iMac-8.attlocal.net>
2018-10-25 19:06:43 -0700

    Done
    done

--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -1,5 +1,26 @@
 package _02_gotta_catchem_all;
 
 public class ExceptionMethods {
-	
+	double divide(double a, double b) throws IllegalArgumentException{
+	if(b==0) {
+		throw new IllegalArgumentException("Can't divide by 0");
+	}
+	else {
+			return a/b;
+	}
+		
+		
+	}
+	String reverseString(String s) throws IllegalStateException {
+		if(s.length()==0) {
+			throw new IllegalStateException();
+		}
+		else {
+			String x= "";
+			for (int i = s.length(); i >0; i--) {
+				x+=s.substring(i-1,i);
+			}
+			return x;
+		}
+	}
 }
--- src/_02_gotta_catchem_all/ExceptionsTest.java
+++ src/_02_gotta_catchem_all/ExceptionsTest.java
@@ -32,7 +32,14 @@ class ExceptionsTest {
 	//3. Complete the JUnit test method to test the divide method.
 	@Test
 	public void testDivideByZeroException() {
-		
+		try {
+			assertEquals(2.5,em.divide(5, 2));
+			em.divide(4, 0);
+			fail("IllegalArgumentException not thrown");
+		}
+		catch(IllegalArgumentException e){
+			e.printStackTrace();
+		}
 	}
 	
 	//4. In the ExceptionMethods class, write a method called reverseString that takes a
@@ -42,7 +49,14 @@ class ExceptionsTest {
 	//5. Complete the JUnit test method to test the reverseStringMethod.
 	@Test
 	public void testReverseString() {
-		
+		try {
+			assertEquals("racecar",em.reverseString("racecar"));
+		em.reverseString("");
+		fail("IllegalStateException not thrown");
+		}
+		catch(IllegalStateException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	
